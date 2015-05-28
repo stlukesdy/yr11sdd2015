90423869f8d5f4319ad4875de2c4cef389a2c93d
MatthiasL <matthias_ludwig@yahoo.com>
2015-05-29 06:03:22 +1000

    16 Sequential files

---
+++ public/16_Sequential_files/program.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+  $("#runButton").click(program);
+});
+
+var program = function () {
+
+  var productArray = [];
+
+  productArray[0] = {};
+  productArray[0].id = 1;
+  productArray[0].name = 'Oven cleaner kit';
+
+  productArray[1] = {};
+  productArray[1].id = 2;
+  productArray[1].name = 'Dishwasher tablets';
+
+
+
+  for (var i = 0; i < 2; i = i + 1) {
+    console.log("id = " + productArray[i].id + " name = " + productArray[i].name);
+  }
+
+};
