a65c1bd4a8f6e53e813098eb4a22e69625c6ac0d
M Ludwig <mludwig@stlukes.nsw.edu.au>
2015-06-04 04:17:47 +0000

    started node

--- public/01_Hello_world/program.js
+++ public/01_Hello_world/program.js
@@ -3,11 +3,28 @@
 });
 
 var program = function () {
-  console.log("Hello world");
-  console.log(5 + 6);
+  // console.log("Hello world");
+  // console.log(5 + 6);
 
-  var number1 = 5;
-  var number2 = 6;
-  var result = number1 + number2;
-  console.log("The result is " + result);
+  // var number1 = 5;
+  // var number2 = 6;
+  // var result = number1 + number2;
+  // console.log("The result is " + result);
+  
+  var names = ['name1', 'name2'];
+  console.log( names.length );
+  
+  names[0] = 'name0';
+  console.log( names[0] );
+  
+  var i = 0;
+  
+  while( i < names.length){
+    console.log( names[i]);
+    i = i + 1;
+  }
+  
+  
+  
+  
 };
